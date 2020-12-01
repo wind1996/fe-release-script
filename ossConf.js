612b5970b907191d9340615271dc2d738e38430b
Good luck <2924456233@qq.com>
2020-12-01 21:41:43 +0800

    [M]buildCmd 配置项支持函数类型的参数
    buildCmd配置项增加函数类型，如果函数为异步则需要通过返回Promise.reslove()来执行下一个函数或者命令

---
+++ ossConf.js
@@ -0,0 +1,9 @@
+module.exports = {
+    ossConf: {
+        region: 'oss-cn-beijing',
+        accessKeyId: 'LTAI4G6pWcBsewTrJ6gwRsJs',
+        accessKeySecret: 'dbQIWETdSRYAWXxDaK7f3iTm0QFMg4',
+        bucket: 'wind1996',
+        timeout: 1000 * 1000,
+    }
+}
