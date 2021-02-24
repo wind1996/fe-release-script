26f5ae9798880153a0dfaa996edeed1f36bb42e5
windy1996 <2924456233@qq.com>
2021-02-24 18:18:56 +0800

    [M]使用commander优化终端交互

--- bin/index.js
+++ bin/index.js
@@ -1,13 +1,36 @@
 #!/usr/bin/env node
-const arg = process.argv.splice(2)
-if (arg.length === 1 && arg[0] === "config") {
-    require('../lib/cmdEntrance/generztorConfigFile');
-} else if (arg.length === 1 && arg[0] === "start") {
-    require('../lib/cmdEntrance/startServerCommand');
-} else if (arg.length === 1 && arg[0] === "init") {
-    require('../lib/cmdEntrance/initCommand');
-} else if (arg.length === 1 && arg[0] === "-v") {
-    require('../lib/cmdEntrance/version');
-} else {
-    require('../lib/cmdEntrance');
-}
+const program = require('commander');
+const {version} = require("../package.json")
+program.version(version);
+program.description('用于项目构建发布的终端工具')
+
+program
+    .command("config")
+    .description("生成配置文件")
+    .action(function () {
+        require('../lib/cmdEntrance/generztorConfigFile');
+    })
+
+program
+    .command("start")
+    .description("执行启动命令")
+    .action(function () {
+        require('../lib/cmdEntrance/startServerCommand');
+    });
+
+program
+    .command("init")
+    .description("初始化目录")
+    .action(function (option) {
+        require('../lib/cmdEntrance/initCommand');
+    });
+
+program
+    .command("deploy")
+    .description("发布项目")
+    .action(function (name,options) {
+        require('../lib/cmdEntrance');
+    })
+
+
+program.parse(process.argv);
